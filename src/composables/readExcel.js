f7d93050ef32194c452cb06809ef1990ddf3ec7a
胡艺博 <3262816046@qq.com>
2026-08-16 16:16:19 +0800

    防数据泄漏

--- src/composables/readExcel.js
+++ src/composables/readExcel.js
@@ -1,9 +1,5 @@
 import { ref } from 'vue'
-  import * as XLSX from 'xlsx'
-
-  const GaoDeKey = '61158e4b00c738a7ce2f1d31b37ed78f'
-
-  // 高德地理编码 API 并发限制，每次请求间隔（毫秒）
-  const REQUEST_DELAY = 120
+import * as XLSX from 'xlsx'
+import { GAODE_KEY } from '../config'
 
   
